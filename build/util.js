00a7cbe1b9c08e17624dc8fa254b93e98cad3dd7
Sam Mear <sam.mear@brandaddition.com>
2025-09-19 14:43:28 +0100

    only include isPart in pure.js

--- build/util.js
+++ build/util.js
@@ -340,11 +340,6 @@ const util = {
 				),
 			),
 		)(yamlObject);
-	},
-
-	isPart: part => {
-		const specTypes = ['CPU', 'Graphics Card', 'APU'];
-		return specTypes.includes(part.type);
 	}
 };
 module.exports = util;
